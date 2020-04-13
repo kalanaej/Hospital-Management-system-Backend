debd416295a98cd63dcb3bf882214f99e8b403a5
kalana.eranda <kalanaejayasuriya@gmail.com>
2020-04-13 13:31:02 +0530

    create dbconnect

--- Hospital/src/doctors/model/Doctor.java
+++ Hospital/src/doctors/model/Doctor.java
@@ -1,5 +1,23 @@
 package doctors.model;
 
-public class Doctor {
+import java.sql.*;
 
+public class Doctor {
+	
+	private Connection connect()
+	{
+		Connection con = null;
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			//Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/hospital", "root", "");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
