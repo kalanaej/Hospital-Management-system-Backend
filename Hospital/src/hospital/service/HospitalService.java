645f7d7dcc4b7f1826938a85c4382d6a48e8e534
Dilushaa <dilushagayanebaynew1@gmail.com>
2020-04-14 02:51:43 +0530

    c#8 implement rest api - put

--- Hospital/src/hospital/service/HospitalService.java
+++ Hospital/src/hospital/service/HospitalService.java
@@ -49,6 +49,30 @@ Hospital hosObj = new Hospital();
 	} 
 	
 
+	@PUT 
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String updateHospitalDet(String hospitalData) 
+	{  
+		//Convert the input string to a JSON object  
+		JsonObject hospitalObject = new JsonParser().parse(hospitalData).getAsJsonObject(); 
+		 
+		 //Read the values from the JSON object  
+		String hospitalID = hospitalObject.get("hospitalID").getAsString(); 
+		String mohCode = hospitalObject.get("mohCode").getAsString();  
+		String hospitalName = hospitalObject.get("hospitalName").getAsString(); 
+		String emailAddress = hospitalObject.get("emailAddress").getAsString();  
+		String managerName = hospitalObject.get("managerName").getAsString();
+		String address = hospitalObject.get("address").getAsString();
+		String telephoneNo = hospitalObject.get("telephoneNo").getAsString();
+		 
+		String output = hosObj.updateHospitalDet(hospitalID, mohCode, hospitalName, emailAddress, managerName,address,telephoneNo); 
+		 
+		 return output; 
+		 
+		 } 
+		 
 	
 
 }
