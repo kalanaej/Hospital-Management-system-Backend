05fff65cda930dbb99b20efe7e4f67140b9752f7
kalana.eranda <kalanaejayasuriya@gmail.com>
2020-04-13 13:59:49 +0530

    added dbconnector

---
+++ Hospital/src/dbconnector/DBConnect.java
@@ -0,0 +1,17 @@
+package dbconnector;
+
+import java.sql.*;
+
+public class DBConnect {
+
+	//A common method to connect to the DB
+	public Connection connect() throws SQLException,ClassNotFoundException
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		//Provide the correct details: DBServer/DBName, username, password
+		Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+		
+		return con;	
+	}
+}
