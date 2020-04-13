b0349decc23c0e52b76db454e6118b7abfcfcb83
Dilushaa <dilushagayanebaynew1@gmail.com>
2020-04-14 01:59:08 +0530

    implement update hospital details

--- Hospital/src/hospital/service/Hospital.java
+++ Hospital/src/hospital/service/Hospital.java
@@ -146,7 +146,57 @@ public String readHospitalDetails()  {
 	  
 	}
 	
-	
+public String updateHospitalDet(String ID, String MOHcode, String Hos_name, String Hos_email, String Hos_managername,  String Hos_address, String Hos_phoneNumber)  
+
+{   String output = ""; 
+ 
+  try   {   
+	  
+	  	DBConnect db = new DBConnect();
+		Connection con = null;
+		con = db.connect(); 
+ 
+   if (con == null)    
+   {
+	   return "Error while connecting to the database for updating."; 
+   
+   } 
+ 
+   // create a prepared statement    
+   String query = "UPDATE hospital SET mohCode=?,hospitalName=?,emailAddress=?,managerName=?,address=?,telephoneNo=?      WHERE hospitalID=?"; 
+ 
+   PreparedStatement preparedStmt = con.prepareStatement(query); 
+ 
+   // binding values    
+   preparedStmt.setString(1, MOHcode);    
+   preparedStmt.setString(2, Hos_name);    
+   preparedStmt.setString(3, Hos_email);   
+   preparedStmt.setString(4, Hos_managername);
+   preparedStmt.setString(5, Hos_address); 
+   preparedStmt.setString(6, Hos_phoneNumber); 
+   preparedStmt.setInt(7, Integer.parseInt(ID)); 
+ 
+   // execute the statement    
+   preparedStmt.execute();    
+   con.close(); 
+ 
+   output = "Updated successfully";   
+   
+  }   
+  
+  catch (Exception e)  
+  {    
+	  output = "Error while updating the hospital details.";    
+	  System.err.println(e.getMessage());   
+	  
+  } 
+ 
+  return output;  
+  
+} 
+ 
+
+
 	
 	
 	
