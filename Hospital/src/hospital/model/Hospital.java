b4e09c21b446041c50c72eddbba27e90b5e41a62
kalana.eranda <kalanaejayasuriya@gmail.com>
2020-04-16 10:34:41 +0530

    Edit sql library

--- Hospital/src/hospital/model/Hospital.java
+++ Hospital/src/hospital/model/Hospital.java
@@ -2,11 +2,7 @@ package hospital.model;
 
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 import dbconnector.DBConnect;
 
