4bd6439a3e5fdd714e577ba9acfe1305f76db2e9
kalana.eranda <kalanaejayasuriya@gmail.com>
2020-04-17 11:48:59 +0530

    fixed some mistakes

--- Hospital/src/user/model/UserLogin.java
+++ Hospital/src/user/model/UserLogin.java
@@ -3,7 +3,6 @@ package user.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 import dbconnector.DBConnect;
